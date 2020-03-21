670f250900beaa8fd2093bcd2d85e2320e978b3c
littletao08 <cvghju@163.com>
2020-03-21 15:38:09 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,9 +6,9 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 #移除不用软件包
-rm -rf k3screenctrl package/lean
+#rm -rf k3screenctrl package/lean
 #添加额外软件包
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
