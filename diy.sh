a0adecc3f61fae717bbff71ba0a7564626e1bab2
littletao08 <cvghju@163.com>
2020-03-28 09:55:45 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,7 +12,5 @@ sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generat
 #添加额外软件包
 git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
-git clone https://github.com/lwz322/luci-app-k3screenctrl.git package/k3/luci-app-k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl.git package/k3/k3screenctrl
-git clone https://github.com/lwz322/k3screenctrl_build.git package/k3/k3screenctrl_build
 git clone https://github.com/pymumu/luci-app-smartdns.git  package/luci-app-smartdns
+git clone https://github.com/littletao08/luci-app-eqos.git package/luci-app-eqos
