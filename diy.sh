62128bf03a5fc9582c8c89c49976d1eb01f4489d
littletao08 <cvghju@163.com>
2020-03-29 11:16:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,9 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
+./scripts/feeds update -a
+./scripts/feeds install -a
 #移除不用软件包
 #rm -rf k3screenctrl package/lean
 #添加额外软件包
