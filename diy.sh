b2f02a0e36dd5313918bf00b9cb6fcb576eef2ad
littletao08 <cvghju@163.com>
2020-04-02 21:45:30 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,7 +15,7 @@ echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.de
 #添加额外软件包
 #git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 #git clone https://github.com/yangsongli/luci-theme-atmaterial.git package/luci-theme-atmaterial
-#git clone https://github.com/pymumu/luci-app-smartdns.git  package/luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git  package/luci-app-smartdns
 git clone https://github.com/littletao08/luci-app-eqos.git package/luci-app-eqos
 #git clone https://github.com/hong0980/luci-app-passwall.git package/luci-app-passwall
 #git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
