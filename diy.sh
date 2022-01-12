99c443059d704a4230c3bbc9ff61b39719bbbba7
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2022-01-12 08:28:53 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
 ./scripts/feeds update -a
 ./scripts/feeds install -a
